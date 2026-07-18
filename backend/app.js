8c3704b5e6222e53a1cf74e9563d1e3394a4218e
abhishekchauhan951757 <Abj=hishek95175755ch@gmail.com>
2026-07-18 23:59:08 +0530

    database connectivity

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,10 @@ const express=require('express');
 const app=express();
 const dotenv=require('dotenv');
 dotenv.config();
+const connectDB=require('./db/db');
+
+connectDB();
+
 const cors=require('cors');
 app.use(cors());
 app.get('/',(req,res)=>{
